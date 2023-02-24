e94ccd73427c045d0ef1033cd5b37e9b16ee0e85
Felipe Aguirre Martinez <felipe@prediktia.ai>
2023-02-24 18:01:20 -0500

    Add pdf plot with contextual information

---
+++ modelui/plot.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import scipy.stats as stats
+
+
+def plot_prediktion_with_matches(prediction, matches):
+    xmin = 0
+    xmax = (
+        max(
+            prediction.recommended_sales,
+            prediction.prediction,
+            prediction.stock_in,
+            matches.recommended_sales.max(),
+        )
+        * 1.1
+    )
+
+    matches = matches.assign(
+        label=lambda df: df.apply(
+            lambda row: f"{int(row.order)}-{row.freeport_sku_match}: {row.score: 0.2f}",
+            axis=1,
+        ),
+    )
+
+    if prediction.std_dev != 0:
+        alpha = (prediction.mean_reco**2) / (prediction.std_dev**2)
+        beta = prediction.mean_reco / (prediction.std_dev**2)
+        scale = 1 / beta
+        matches = matches.assign(
+            pdf=lambda df: stats.gamma.pdf(df.recommended_sales, a=alpha, scale=scale),
+        )
+    else:
+        matches = matches.assign(pdf=0)
+
+    fig = px.scatter(
+        matches,
+        x="recommended_sales",
+        y="pdf",
+        hover_data=["freeport_sku_match", "score", "order", "brand"],
+        hover_name="label",
+        text="label",
+        symbol_sequence=["x"],
+    ).update_traces(mode="markers+text", marker_size=10, textposition="top center")
+
+    if prediction.std_dev != 0:
+        df_dist = (
+            pd.Series(np.linspace(xmin, xmax, 100), name="x")
+            .to_frame()
+            .assign(y=lambda df: stats.gamma.pdf(df.x, a=alpha, scale=scale))
+        )
+
+        fig.add_trace(
+            px.line(
+                df_dist,
+                x="x",
+                y="y",
+                hover_data={
+                    "x": False,
+                    "y": False,
+                },
+            ).data[0]
+        )
+    fig.add_vrect(
+        x0=xmin,
+        x1=prediction.recommended_sales,
+        annotation_text=f"underbuy: {prediction.underbuy_cost.round()}",
+        annotation_position="bottom left",
+        fillcolor="orange",
+        opacity=0.25,
+        line_width=0,
+    )
+    fig.add_vrect(
+        x0=prediction.recommended_sales,
+        x1=xmax,
+        annotation_text=f"overbuy: {prediction.overbuy_cost.round()}",
+        annotation_position="bottom right",
+        fillcolor="blue",
+        opacity=0.25,
+        line_width=0,
+    )
+    fig.add_vline(
+        x=prediction.recommended_sales,
+        line_width=3,
+        line_dash="dash",
+        line_color="green",
+        annotation_text=f"Recommended Sales: {prediction.recommended_sales}",
+        annotation_position="top right",
+        annotation_textangle=90,
+    )
+    fig.add_vline(
+        x=prediction.prediction,
+        line_width=2,
+        line_dash="dot",
+        line_color="red",
+        annotation_text=f"Prediktion: {prediction.prediction} - Expected cost of error {prediction.lost_prediktia.round()}",
+        annotation_position="top right",
+        annotation_textangle=90,
+    )
+    fig.add_vline(
+        x=prediction.stock_in,
+        line_width=2,
+        line_dash="dot",
+        line_color="red",
+        annotation_text=f"Human: {prediction.stock_in} - Expected cost of error {prediction.lost_human.round()}",
+        annotation_position="top right",
+        annotation_textangle=90,
+    )
+    return fig
--- pages/01_Analyze_prediction.py
+++ pages/01_Analyze_prediction.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
-from modelui import app, data
+from modelui import data, plot
+
+
+@st.cache_data(persist=True)
+def load_thumbnail(sku):
+    return data.load_thumbnail(sku=sku)
+
+
+def display_matches(df_matches):
+    for _, match in df_matches.iterrows():
+        col_image, col_table = st.columns([1, 4])
+        col_image.image([load_thumbnail(sku=match.freeport_sku_match)])
+
+        col_table.write(
+            match.drop(["image_uri", "freeport_sku", "closest_image_filename", "brand"])
+            .rename(match.freeport_sku_match)
+            .to_frame()
+            .T.to_html(escape=False),
+            unsafe_allow_html=True,
+        )
+
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
@@ -11,8 +31,20 @@ if __name__ == "__main__":
     # SIDEBAR
     st.title("Target image: Shoe for which we are predicting")
     sku = st.sidebar.selectbox("SKU", df_predictions.index.drop_duplicates().to_list())
-    app.display_target_shoe(df_predictions.loc[sku])
+    prediction = df_predictions.loc[sku]
+    matches = df_matches.query(f"freeport_sku == '{sku}'")
+
+    st.dataframe(prediction.rename(sku).to_frame().T)
+    col_image, col_plot = st.columns([1, 6])
+    col_image.image([load_thumbnail(sku=sku).resize((170, 170))])
+    col_plot.plotly_chart(
+        plot.plot_prediktion_with_matches(
+            prediction=prediction,
+            matches=matches,
+        ),
+        use_container_width=True,
+    )
 
     # SIDEBAR
     st.title("Target matches: Matched products from previous season")
-    app.display_matches(df_matches.query(f"freeport_sku == '{sku}'"))
+    display_matches(matches)
